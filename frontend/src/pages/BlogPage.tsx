3cd6090b3591e823113829f0c25130e76516df86
Harsh Mahadev Duche <ducheharsh@gmail.com>
2024-05-20 12:45:10 +0530

    minor changes

--- frontend/src/pages/BlogPage.tsx
+++ frontend/src/pages/BlogPage.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { Blog } from "../components/Blog"
+
 
 interface BlogType {
     id?: number;
--- frontend/src/pages/CreateBlog.tsx
+++ frontend/src/pages/CreateBlog.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export function CreateBlog(){
